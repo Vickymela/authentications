66d1a386f80b4866702d64eb093c7e274060f1a3
pamela <pamelavictory70@gmail.com>
2025-11-17 16:39:47 +0100

    last

--- formapp/views.py
+++ formapp/views.py
@@ -20,8 +20,8 @@ def RegisterView(request):
     
 
     if request.method == "POST":
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
+        # first_name = request.POST.get('first_name')
+        # last_name = request.POST.get('last_name')
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
@@ -44,8 +44,8 @@ def RegisterView(request):
             return redirect('register')
         else:
             new_user = User.objects.create_user(
-                first_name=first_name,
-                last_name=last_name,
+                # first_name=first_name,
+                # last_name=last_name,
                 email=email, 
                 username=username,
                 password=password
